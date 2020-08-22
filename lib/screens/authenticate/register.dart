ce14d73ee2b6f59fc43405a34a9a064d02d3ff4f
Arun Regmi <aregmi450@gmail.com>
2020-08-22 14:35:00 +0545

    fourth commit

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -23,72 +23,78 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return loading ?Loading():  Scaffold(
-      backgroundColor: Colors.red[200],
-      appBar: AppBar(
-        backgroundColor: Colors.red[400],
-        elevation: 0.0,
-        title: Text('Sign Up to Tabler'),
-        actions: <Widget>[
-          FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('Sign In'),
-              onPressed: () {
-                widget.toggleView();
-              })
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 11.0),
-              TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  }),
-              SizedBox(height: 7.0),
-              TextFormField(
-                  decoration:
-                      textInputDecoration.copyWith(hintText: 'Password'),
-                  obscureText: true,
-                  validator: (val) =>
-                      val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  }),
-              SizedBox(height: 11.0),
-              RaisedButton(
-                  color: Colors.green[600],
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      setState(() => loading = true);
-                      dynamic result = await _auth.registerWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() => error = 'enter your valid email';
-                        loading =false;
-                      });
-                    }
-                  }
-  }),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.red[200],
+            appBar: AppBar(
+              backgroundColor: Colors.red[400],
+              elevation: 0.0,
+              title: Text('Sign Up to Tabler'),
+              actions: <Widget>[
+                FlatButton.icon(
+                    icon: Icon(Icons.person),
+                    label: Text('Sign In'),
+                    onPressed: () {
+                      widget.toggleView();
+                    })
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 11.0),
+                    TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        }),
+                    SizedBox(height: 7.0),
+                    TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        obscureText: true,
+                        validator: (val) => val.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        }),
+                    SizedBox(height: 11.0),
+                    RaisedButton(
+                        color: Colors.green[600],
+                        child: Text(
+                          'Register',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .registerWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() {
+                                error = 'enter your valid email';
+                                loading = false;
+                              });
+                            }
+                          }
+                        }),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
   }
 }
