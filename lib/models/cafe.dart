ee65879b65609495e03e4c6633b166da74dec7fd
Arun Regmi <aregmi450@gmail.com>
2020-08-25 08:44:15 +0545

    commit

---
+++ lib/models/cafe.dart
@@ -0,0 +1,7 @@
+class Cafe {
+  final String cafename;
+  final String location;
+  final int contact;
+
+  Cafe({this.cafename, this.location, this.contact});
+}
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,4 @@
+import 'package:app/models/cafe.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -8,12 +9,27 @@ class DatabaseService {
   final CollectionReference cafeCollection =
       Firestore.instance.collection('cafe');
 
-  Future<void> updateUserData(
-      String cafename, String location, int contact) async {
+  Future updateUserData(String cafename, String location, int contact) async {
     return await cafeCollection.document(uid).setData({
       'cafename': cafename,
       'location': location,
       'contact': contact,
     });
+  }
+
+  // cafe list from snapshot
+  List<Cafe> _cafelistFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Cafe(
+        cafename: doc.data['cafename'] ?? '',
+        location: doc.data['location'] ?? '',
+        contact: doc.data['contact'] ?? 0,
+      );
+    }).toList();
+  }
+
+// get cafe data
+  Stream<List<Cafe>> get cafes {
+    return cafeCollection.snapshots().map(_cafelistFromSnapshot);
   }
 }
