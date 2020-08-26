3dfb667ce75159edf4947568d99425b3d7172424
Arun Regmi <46434570+aregmi450@users.noreply.github.com>
2020-08-26 18:04:17 +0545

    fourth commit

---
+++ lib/screens/home/cafe_list.dart
@@ -0,0 +1,23 @@
+import 'package:app/models/cafe.dart';
+import 'package:app/screens/home/cafe_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CafeList extends StatefulWidget {
+  @override
+  _CafeListState createState() => _CafeListState();
+}
+
+class _CafeListState extends State<CafeList> {
+  @override
+  Widget build(BuildContext context) {
+    final cafes = Provider.of<List<Cafe>>(context) ?? [];
+
+    return ListView.builder(
+      itemCount: cafes.length,
+      itemBuilder: (context, index) {
+        return CafeTile(cafe: cafes[index]);
+      },
+    );
+  }
+}
