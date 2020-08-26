3a2d87fc9aac48f614273336a7906471c0cae5dd
Arun Regmi <46434570+aregmi450@users.noreply.github.com>
2020-08-26 18:06:56 +0545

    fifth commit

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,3 +1,4 @@
+import 'package:app/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:app/models/user.dart';
 
@@ -11,7 +12,10 @@ class AuthService {
 
   // auth change user stream
   Stream<User> get user {
-    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+    return _auth.onAuthStateChanged
+        .
+        //.map(_userFromFirebaseUser user) => _userfromFirebaseUser(user));
+        map(_userFromFirebaseUser);
   }
 
   // sign in anon
@@ -27,8 +31,34 @@ class AuthService {
   }
 
 // sign in with email & password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
 // register with email & password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser cafe = result.user;
+
+      // create a new document for the user with the uid
+      await DatabaseService(uid: cafe.uid)
+          .updateUserData('new cafe name', 'new location', 01488546, 0);
+      return _userFromFirebaseUser(cafe);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
 // sign out
   Future signOut() async {
