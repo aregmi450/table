86d98be65a18facfce57470e7b8e8767ab353ecc
Arun Regmi <46434570+aregmi450@users.noreply.github.com>
2020-08-26 18:03:36 +0545

    Update home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,28 +1,51 @@
+import 'package:app/models/cafe.dart';
+import 'package:app/screens/home/cafe_list.dart';
+import 'package:app/screens/home/settings_form.dart';
 import 'package:app/services/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:app/services/database.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red[50],
-      appBar: AppBar(
-        title: Text('Tabler'),
-        backgroundColor: Colors.red[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('logout'),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ],
-      ),
-    );
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: SettingsForm(),
+            );
+          });
+    }
+
+    return StreamProvider<List<Cafe>>.value(
+        value: DatabaseService().cafes,
+        child: Scaffold(
+          backgroundColor: Colors.red[50],
+          appBar: AppBar(
+            title: Text('Tabler'),
+            backgroundColor: Colors.red[400],
+            elevation: 0.0,
+            actions: <Widget>[
+              FlatButton.icon(
+                icon: Icon(Icons.person),
+                label: Text('logout'),
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+              ),
+              FlatButton.icon(
+                icon: Icon(Icons.settings),
+                label: Text('Settings'),
+                onPressed: () => _showSettingsPanel(),
+              ),
+            ],
+          ),
+          body: CafeList(),
+        ));
   }
 }
